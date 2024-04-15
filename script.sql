607125115b9978b6ce6978901746dcdbc696c0db
mav-26 <calegarmarcus@gmail.com>
2024-04-15 10:12:08 -0300

    modificacao script.sql

--- script.sql
+++ script.sql
@@ -5,12 +5,12 @@ CREATE TABLE Aluno (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(150) not null,
     email varchar(100),
-    sexo char(1) not null,
+    sexo varchar(9) not null,
     endereco varchar(255) ,
     numeroCasa int not null,
-    complemento varchar(15),
+    complemento varchar(30),
     bairro varchar(50) not null,
     cidade varchar(50) not null,
     uf char(2) not null,
-    modalidade varchar(25) not null
+    modalidade varchar(50) not null
 )  ENGINE=INNODB;
